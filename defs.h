39280d1fe0d835345b8c8b8ec079c0e587ea909d
nhatdoankhanh <nhatxuanx31@gmail.com>
2024-03-24 23:52:05 +0700

    Initial commit

---
+++ defs.h
@@ -0,0 +1,11 @@
+#ifndef _DEFS__H
+#define _DEFS_H
+
+#define SCREEN_HEIGHT 600
+
+#define SCREEN_WIDTH 800
+
+#define WINDOW_TITLE "Hello World!"
+
+#define BACKGROUND_IMG "background.jpg"
+#endif // _DEFS__H
